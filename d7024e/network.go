6f83c60f04b26cfa96ed3a122c263dfb532472dd
David Olsson Granlund <david.olsson-granlund@arcticgroup.se>
2018-09-28 09:05:01 +0200

    added initiator and moved main code to it

--- d7024e/network.go
+++ d7024e/network.go
@@ -36,13 +36,22 @@ func Listen(ip string, port int) {
 		rpc := &RPC{}
 		err = proto.Unmarshal(buf[0:n], rpc)
 
-		switch rpc.RpcType {
-		case 1:
-			log.Printf("Received PING from %s\n", hex.EncodeToString(rpc.SenderId))
+		rpc.SenderIp = addr.String()
+		//fmt.Println(rpc.SenderIp)
+		Requests <- *rpc
+		/*
+			switch rpc.RpcType {
+			case 1:
+				//log.Printf("Received PING from %s\n", hex.EncodeToString(rpc.SenderId))
+				Requests <- "PING"
+				/*contact := NewContact(IdFromBytes(rpc.SenderId), addr.String())
+				fmt.Println(contact.String())
+				// add contact to routing table
 
-		default:
-			log.Printf("Received something else from addr: %s", addr)
-		}
+			default:
+				Requests <- "OTHER"
+				log.Printf("Received something else from addr: %s", addr)
+			}*/
 
 		CheckError(err)
 	}
