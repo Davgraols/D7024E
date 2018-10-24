3149723a8ee78dc3b00eb5157bc3aef078758c3a
Erik Gustavsson <eg_nsk@hotmail.com>
2018-10-24 15:33:30 +0200

    more unitests

--- d7024e/kademliaID_test.go
+++ d7024e/kademliaID_test.go
@@ -10,6 +10,33 @@ func testNewRandomKademliaID(t *testing.T) {
 
 	if testone == testtwo {
 
-		t.Errorf("error, generate two same ID. ")
+		t.Errorf("error, generate two of the same ID. ")
+	}
+}
+
+func testNewRandomSerial(t *testing.T) {
+	testone := NewRandomSerial()
+	testtwo := NewRandomSerial()
+
+	if testone == testtwo {
+
+		t.Errorf("error, generate two of the same Serial. ")
+	}
+}
+
+func testNewRandomHash(t *testing.T) {
+	testone := NewRandomHash("1337")
+	testtwo := NewRandomHash("1337")
+
+	if testone == testtwo {
+
+		t.Errorf("error, generate two of the same hash. ")
+	}
+}
+
+func testEquals(t *testing.T) {
+	node := NewRandomKademliaID()
+	if !(node.Equals(node)) {
+		t.Errorf("the Equals du not return True if the nodes are equal")
 	}
 }
